9a59b5dcbfe5432543c6c24f43d80a228cc1f7e8
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-29 17:33:20 +0800

    Delete Adv directory

--- Adv/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Date    : 2/16/21
-# @Author  : Xinyu Gong (xinyu.gong@utexas.edu)
-# @Link    : None
-# @Version : 0.0
--- Adv/scripts/test_saauxbn.sh
+++
@@ -1,3 +0,0 @@
-python test.py --data ~/dataset --batch_size 128\
-	--test_eps 8 --test_gamma 2 --test_steps 20 --test_randinit \
-	--norm_module saauxbn --weight_path output/saauxbn/model_epoch190.pt
