cbd6641275b951c13415b2c8873d5d4d150aea2c
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-29 17:42:45 +0800

    Delete NST directory

--- NST/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Date    : 2/16/21
-# @Author  : Xinyu Gong (xinyu.gong@utexas.edu)
-# @Link    : None
-# @Version : 0.0
-
-from .network import AdaINNet, SaAdaINNet
-from .modules import vgg, decoder
--- NST/scripts/saadain_net.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-python train.py --content_dir ~/dataset/coco/images/train2017 --style_dir ~/dataset/paint/train_1 \
-    --val_content_dir ~/dataset/coco/images/val2017 --val_style_dir ~/dataset/paint/test \
-    --vgg pretrained_weights/vgg_normalised.pth --saadain --save_dir experiments/saadain
